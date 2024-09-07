761c73f08752e52f46d950a628121b76b39270b3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-09-07 23:36:54 +0600

    input

--- 1.InputOutput/1.9.swift
+++ 1.InputOutput/1.9.swift
@@ -1,2 +1,13 @@
 // 1.9. Write a program that read any lower case character and display in upper case
 
+// Prompt the user to enter aa lowercase character
+print("please enter a lowercase character: ")
+
+if let input = readLine(), let character = input.first {
+        let upercaseChar = character.toUpper
+
+        print("uppercase of input character is: \(upercaseChar)" )
+        
+} else {
+        print("Error!!!, please enter lower case character")
+}
