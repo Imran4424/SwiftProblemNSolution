465717717d78568fccd595871542277178cfdd69
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-12-20 23:11:20 +0600

    swift print

--- 1.InputOutput/1.1.swift
+++ 1.InputOutput/1.1.swift
@@ -8,7 +8,7 @@ print("SHAH MD IMRAN HOSSAIN")
         print() function signature:
         print(_ items: Any..., separator: String = " ", terminator: String = "\n")
 
-        1. String items: it's plural, it can be multiple items
+        1. items: it's plural, it can be multiple items
         2. separator: separator string of multiple items, default parameter value of this parameter is single space
         3. terminator: what string should be appended after the given items, efault parameter value of this parameter is single line break(\n)
 */
