c915bfbb13605beda5737f9ea49e484fb60a84ff
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-12-20 23:07:21 +0600

    swift

--- 1.InputOutput/1.1.swift
+++ 1.InputOutput/1.1.swift
@@ -1,3 +1,21 @@
 // 1.1. Write a program that prints your name
 
 print("SHAH MD IMRAN HOSSAIN")
+
+/* 
+        print() - is the basic output function of swift
+
+        print() function signature:
+        print(_ items: Any..., separator: String = " ", terminator: String = "\n")
+
+        1. String items: it's plural, it can be multiple items
+        2. separator: separator string of multiple items, default parameter value of this parameter is single space
+        3. terminator: what string should be appended after the given items, efault parameter value of this parameter is single line break(\n)
+*/
+
+
+// single line comment
+
+/* 
+        Multiline comment
+*/
