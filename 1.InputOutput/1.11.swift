a1744b9f9eb44300002937ed03396003d324fca7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-12-21 19:31:49 +0600

    swift input and output

--- 1.InputOutput/1.11.swift
+++ 1.InputOutput/1.11.swift
@@ -1,1 +1,10 @@
-1.11. Write a program that declares and uses any Bool character
+// 1.11. Write a program that declares and uses any Bool character
+
+/* 
+        a boolean data type used to represent logical entities
+
+        Boolean data types have only two possible values
+                True
+                False
+*/
+
