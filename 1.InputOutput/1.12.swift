8e65593138a52274cf48145fefaa44dd3ce46b3b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-12-21 23:36:40 +0600

    swift input and output

--- 1.InputOutput/1.12.swift
+++ 1.InputOutput/1.12.swift
@@ -1,1 +1,27 @@
-// 
+// 1.12. Define a variable that can hold either a value or nil. Print the value of the variable
+
+/* 
+        Swift Language has six fundamentals Data types
+
+                1. Int
+                2. Float
+                3. Double
+                4. Character
+                5. Bool
+                6. String
+
+        But there is another data type, but this not exacty data type more like a wrapper of all fundamental 
+        or user defined data type
+
+        This wrapper data type is called Optionals
+
+        Optionals are special type of data types which may contain a value or not (value absent or null)
+*/
+
+/*  
+        We can simply declare a data type as optional by simply appending either of these symbols (? or !)
+
+        ? - If we declare optionals using this symbol, that means this optional can have either a value or nil
+        ! - If we declare optionals using this symbol, that means even if this optional primarily have nil 
+            but it is guaranteed that in future it will have a value
+*/
