f45494c25079a371884b50031afd2274b2b6805d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-12-22 23:04:24 +0600

    swift input and output

--- 1.InputOutput/1.12.swift
+++ 1.InputOutput/1.12.swift
@@ -36,3 +36,30 @@ var intAnotherOptional: Int!
 // both of them have nil
 print(intOptional)
 print(intAnotherOptional)
+
+
+// now assing values to optional variables
+intOptional = 5
+intAnotherOptional = 5
+
+
+// intOptional will show now - Optional(5)
+// since this is an optional variable
+print(intOptional)
+
+
+// intAnotherOptional will show now - 5
+// but why this is showing 5 and not showing Optional(5) like the other optional variable
+// we will get to that later
+print(intAnotherOptional)
+
+/* 
+        As I said earlier, Optionals are actually wrapper of Swift data types (Both fundamentals and user defined)
+        
+        So, we need to unwrap the optional variables to find the actual value
+
+        we can forefully unwrap a optional variable using ! symbol
+*/
+
+// we are unwrapping 
+print(intOptional!)
