b4de1460ddfd322cbebee7a459a0f3ecba2ef203
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-12-25 00:15:12 +0600

    input output

---
+++ 1.InputOutput/1.17.swift
@@ -0,0 +1,19 @@
+// 1.17. Write a program to access the max in min value of literals
+
+let intMaxValue = Int.max
+let intMinValue = Int.min
+
+print("Int max value", intMaxValue)
+print("Int min value", intMinValue)
+
+let floatMaxValue = Float.max
+let floatMinValue = Float.min
+
+print("Float max value", floatMaxValue)
+print("Float min value", floatMinValue)
+
+let doubleMaxValue = Double.max
+let doubleMinValue = Double.min
+
+print("Double max value", doubleMaxValue)
+print("Double min value", doubleMinValue)
