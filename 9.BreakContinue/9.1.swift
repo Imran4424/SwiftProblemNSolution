736ac90b163d589969beb4ae5fb6da3da56eb0a6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-01-29 23:57:40 +0600

    break and continue

--- 9.BreakContinue/9.1.swift
+++ 9.BreakContinue/9.1.swift
@@ -1,1 +1,17 @@
 // 9.1. Write a program that finds the first multiple of 7 between 50 and 100.
+
+var firstMultiple: Int = -1
+
+for value in 50...100 {
+        if value % 7 == 0 {
+                firstMultiple = value
+                break
+        }
+}
+
+if firstMultiple != -1 {
+        print("Found first multiple is: ", firstMultiple)
+} else {
+        print("Not Found")
+        
+}
