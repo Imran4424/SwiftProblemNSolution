2275e2e5570725fe16a6b50f94673e414021f788
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-05-06 21:57:28 +0600

    loop

--- 9.BreakContinue/9.1.swift
+++ 9.BreakContinue/9.1.swift
@@ -5,7 +5,21 @@ var firstMultiple: Int = -1
 for value in 50...100 {
         if value % 7 == 0 {
                 firstMultiple = value
+                // break always break the immediate loop
                 break
+        }
+}
+
+// in this loop 
+for _ in 1...5 { 
+        // this loop is identical of the previous loop
+        for value in 50...100 {
+                if value % 7 == 0 {
+                        firstMultiple = value
+                        // break always break the immediate loop
+                        // it will not break the outer loop
+                        break
+                }
         }
 }
 
@@ -13,5 +27,4 @@ if firstMultiple != -1 {
         print("Found first multiple is: ", firstMultiple)
 } else {
         print("Not Found")
-        
 }
