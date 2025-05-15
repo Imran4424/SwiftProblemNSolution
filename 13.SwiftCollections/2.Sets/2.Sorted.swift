bda64aaff4c08d3a3bacf4b38ffb5963477b8266
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2025-05-16 01:10:36 +0600

    sets

---
+++ 13.SwiftCollections/2.Sets/2.Sorted.swift
@@ -0,0 +1,9 @@
+
+// create a set of integer type
+var studentID : Set<Int> = [112, 114, 116, 118, 115, 121, 131]
+
+print("Student ID: \(studentID)")
+
+// sort this set elements
+studentID.sorted()
+print("Sorted Student ID: \(studentID)")
