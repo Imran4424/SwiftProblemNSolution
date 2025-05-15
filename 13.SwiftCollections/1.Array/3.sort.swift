682b46f2c3a0e6400329bd5f2306649b3fe2d4ad
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2025-05-16 00:40:02 +0600

    array

---
+++ 13.SwiftCollections/1.Array/3.sort.swift
@@ -0,0 +1,28 @@
+
+var array: [Int] = []
+
+for _ in 0..<10 {
+    // using random variables instead of console input
+    array.append(Int.random(in: 0...100))
+}
+
+print("Unsorted Array:", array)
+
+// sorting the array in ascending order using sort function
+array.sort()
+
+print("Array in ascending order:", array)
+
+// reverse the array for decending order
+array.reverse()
+
+print("Array in decending order:", array)
+
+// for re-sorting in ascending order
+array.sort()
+print("Re-sorted Array in ascending order:", array)
+
+// sorting the array in decending order using sort function
+array.sort(by: >)
+
+print("Array in decending order:", array)
