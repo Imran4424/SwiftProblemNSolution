61a9d73bef0979f36b60f6e97bc4672e86ab2108
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2025-05-16 01:17:13 +0600

    sets

--- 13.SwiftCollections/2.Sets/2.Sorted.swift
+++ 13.SwiftCollections/2.Sets/2.Sorted.swift
@@ -5,5 +5,11 @@ var studentID : Set<Int> = [112, 114, 116, 118, 115, 121, 131]
 print("Student ID: \(studentID)")
 
 // sort this set elements
-studentID.sorted()
-print("Sorted Student ID: \(studentID)")
+// Set is an unordered dataset
+// that's why we can not sort it internally
+// when we call the method sorted()
+// it put the elements of the set in an array then sort the array
+// then sorted() method returns the sorted array
+var sortedStudentID = studentID.sorted()
+print("Sorted Student ID: \(sortedStudentID)")
+
