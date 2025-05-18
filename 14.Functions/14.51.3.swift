1ebfaaa9e940a1b1ed102ac8f74868b6902279c0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-05-19 01:17:40 +0600

    functions

--- 14.Functions/14.51.3.swift
+++ 14.Functions/14.51.3.swift
@@ -3,22 +3,22 @@
 func changeValue(x: inout Int, y: inout Int) {
         // the following line will not give compilation error
         // why?
-        // cause inout keyword makes the parameter reference type
+        // cause inout keyword makes the parameter reference type (C++ pointer)
         x = 5
 
         // the following line will not give compilation error
         // why?
-        // cause inout keyword makes the parameter reference type
+        // cause inout keyword makes the parameter reference type (C++ pointer)
         y = 3
 }
 
 
 
-let imran = 0
-let pritha = 0
+var imran = 0
+var pritha = 0
 
 // the following line will give compilation error
-// we can not send constant(immutable) variable as inout function parameter because that value may change
-changeValue(x: imran, y: pritha)
+// cause we are sending value but it wants variable address
+changeValue(x: &imran, y: &pritha)
 
 print("Sum of the entered numbers is", imran + pritha)
