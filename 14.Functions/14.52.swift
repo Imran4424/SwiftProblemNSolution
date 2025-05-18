58af11eea5226c9f4ba320ff6648d0fa6e8b483e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-05-19 01:20:48 +0600

    functions

---
+++ 14.Functions/14.52.swift
@@ -0,0 +1,21 @@
+// 14.52. Demonstrate how to omit the function parameter label
+
+func addition(_ x: Int, _ y: Int) -> Int {
+        return x + y
+}
+
+// promt for entering first Interger
+print("Please enter the first integer:", terminator: " ")
+
+// using Nil-Coalescing Operator for both optionals
+let imran = Int(readLine() ?? "0") ?? 0
+
+// promt for entering first Interger
+print("Please enter the second integer:", terminator: " ")
+
+// using Nil-Coalescing Operator for both optionals
+let pritha = Int(readLine() ?? "0") ?? 0
+
+let sakib = addition(x: imran, y: pritha)
+
+print("Sum of the entered numbers is", sakib)
