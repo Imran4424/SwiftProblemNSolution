a347663418b40b7ff4ba8c36f5f8215dcb9def37
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-05-19 01:34:44 +0600

    functions

---
+++ 14.Functions/14.54.swift
@@ -0,0 +1,20 @@
+// 14.54. Demonstrate multiple return type in swift
+
+func compute(number: Int) -> (Int, Int, Int) {
+
+        let square = number * number
+
+        let cube = square * number
+
+        // returning three values
+        // given number
+        // squre of given number
+        // cube of given number
+        return (number, square, cube)
+}
+
+var result = compute(number: 5)
+
+print("Number:", result.0)
+print("Square:", result.1)
+print("Cube:", result.2)
