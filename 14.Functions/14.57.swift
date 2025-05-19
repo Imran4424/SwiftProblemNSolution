4d21622afb9de1ca4f4fde876b79ace6a5b3ac7c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-05-19 23:57:49 +0600

    functions

--- 14.Functions/14.57.swift
+++ 14.Functions/14.57.swift
@@ -2,3 +2,18 @@
 
 // In Swift, a function can exist inside the body of another function. 
 // This is called a nested function.
+
+// outer function
+func greetMessage() {
+        
+        // inner function
+        func displayName() {
+                print("Good Morning Abraham!")
+        }
+
+        // calling inner function
+        displayName()
+}
+
+// calling outer function
+greetMessage()
