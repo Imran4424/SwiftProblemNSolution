db6f9bca710c28f2e8af06490b96feb7470aeff3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-01 00:48:50 +0600

    higher order functions

--- 17.HigherOrderFunctions/3.Filter/8.Split.swift
+++ 17.HigherOrderFunctions/3.Filter/8.Split.swift
@@ -1,10 +1,21 @@
 
-let anotherMessage = "This message is going to be broken in pieces!"
+let message = "This message is going to be broken in pieces!"
 
-let splitOnce = anotherMessage.split(maxSplits: 1, whereSeparator: { $0 == " " })
+let splitOnce = message.split(maxSplits: 1, whereSeparator: { $0 == " " })
 
 print(splitOnce)
 // Prints ["This", "message is going to be broken in pieces!"]
 
 let anotherMessage = "This message is going to    be broken in pieces!"
 
+let emptySequences = anotherMessage.split(omittingEmptySubsequences: false, whereSeparator: { $0 == " " })
+
+print(emptySequences)
+// Prints ["This", "message", "is", "going", "to", "", "", "", "be", "broken", "in", "pieces!"]
+
+let paragraph = "This paragraph will be separated in parts. Based on the periods and the exclamation mark! There will be three strings!"
+
+let sentences = paragraph.split { $0 == "." || $0 == "!" }
+
+print(sentences)
+// Prints ["This paragraph will be separated in parts", " Based on the periods and the exclamation mark", " There will be three strings"]
