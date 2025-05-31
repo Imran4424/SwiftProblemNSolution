10080ffa7a45f5b1810614a51e0c6c2355b9965f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-05-31 23:36:54 +0600

    higher order functions

--- 17.HigherOrderFunctions/3.Filter/8.SplitWhile.swift
+++ 17.HigherOrderFunctions/3.Filter/8.SplitWhile.swift
@@ -2,7 +2,7 @@
 let sentence = "I love being an Apple ECO system programmer"
 
 let result = sentence.split(whereSeparator: { letter in
-        return element == " "
+        return letter == " "
 })
 
 print("returned collection which satisfy the given condition:", result)
