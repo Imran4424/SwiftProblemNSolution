9a88e28add9a184331e7636007ee577584761759
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-05-31 13:45:29 +0600

    higher order functions

---
+++ 17.HigherOrderFunctions/3.Filter/1.Filter.swift
@@ -0,0 +1,5 @@
+
+// Filter is another most used higher order function
+
+// Filter higher order functions filters data of collection based on the given condition
+// And create a new collection and return it 
