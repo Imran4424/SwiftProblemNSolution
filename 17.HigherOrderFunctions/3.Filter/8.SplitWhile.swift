ac36019afffd1a7a2d601191faeee75ef203beb7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-05-31 23:33:54 +0600

    higher order functions

--- 17.HigherOrderFunctions/3.Filter/8.SplitWhile.swift
+++ 17.HigherOrderFunctions/3.Filter/8.SplitWhile.swift
@@ -1,1 +1,8 @@
 
+let sentence = "I love being an Apple ECO system programmer"
+
+let result = sentence.prefix(while: { element in
+        return element.count > 4
+})
+
+print("returned collection which satisfy the given condition:", result)
