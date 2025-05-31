d060f81716ef3261b8371aeaa1ed9e8d1e5a6cfb
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-05-31 13:33:45 +0600

    higher order functions

--- 17.HigherOrderFunctions/2.Map/3.FlatMap.swift
+++ 17.HigherOrderFunctions/2.Map/3.FlatMap.swift
@@ -1,1 +1,10 @@
 
+// The flatMap function allows us to transform a set of arrays into a single set of array that contains all of the elements.
+
+let dimentions2D = [[3, 4, 5], [2, 5, 3], [1, 2, 2], [5, 5, 4], [3, 5, 3]]
+
+print("Dimentions in 2D", dimentions2D)
+
+let points = dimentions2D.flatMap { $0 }
+
+print("Points in 1D", points)
