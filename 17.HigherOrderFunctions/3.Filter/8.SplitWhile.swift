ac6640748f7a75b35e2e17a5bcf3b6b75538095d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-05-31 23:37:11 +0600

    higher order functions

--- 17.HigherOrderFunctions/3.Filter/8.SplitWhile.swift
+++ 17.HigherOrderFunctions/3.Filter/8.SplitWhile.swift
@@ -1,4 +1,7 @@
 
+// Define a constant string containing a sentence with words separated by spaces.
+// We will use this sentence to demonstrate how `split(whereSeparator:)` works
+// by breaking it into substrings wherever a specified separator condition is met.
 let sentence = "I love being an Apple ECO system programmer"
 
 let result = sentence.split(whereSeparator: { letter in
