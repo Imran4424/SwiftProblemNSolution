71c5bfbc2150d7ef43006b9618f77025dab4fd6b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-01 23:44:50 +0600

    dictionary

--- 13.SwiftCollections/3.Dictionary/1.Dictionary.swift
+++ 13.SwiftCollections/3.Dictionary/1.Dictionary.swift
@@ -15,3 +15,71 @@
 // value is the capital name
 var capitalCity = ["Nepal": "Kathmandu", "Italy": "Rome", "England": "London"]
 print(capitalCity)
+
+// add elements to dictionary
+capitalCity["Bangladesh"] = "Dhaka"
+print(capitalCity)
+
+capitalCity["Canada"] = "Ottowa city"
+
+for (key, value) in capitalCity {
+        print("\(key): \(value)")
+}
+print("")
+
+
+// changing value in a dictionary
+capitalCity["Canada"] = "Ottowa"
+
+for (country, capital) in capitalCity {
+        print("\(country): \(capital)")
+}
+print("")
+
+// dictionary with keys and values of different data types
+var numbers = [1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six"]
+print(numbers)
+
+// access only keys
+var numberKeys = Array(numbers.keys)
+print("keys:", numberKeys)
+
+// access values only
+var numberValues = Array(numbers.values)
+print("values", numberValues)
+
+// declaring an empty dictionary
+var sortedNumbers: [Int: String] = [:]
+
+// since dictionary is unordered collection
+// sorted function will provide the sorted array of dictionary elements
+let sortedNumbersArray = numbers.sorted { $0.key < $1.key }
+print(sortedNumbersArray)
+
+// contains will provide a boolean response if the key is available or not in the dictionary
+// contains require a key as parameter
+// it will return true if the provided key is present in the dictionary
+// it will return false if the provided key is not present in the dictionary
+var isPresent = numbers.keys.contains(6)
+print("Is 6 present in dictionary", isPresent)
+
+// delete a key from dictionary
+let removedNumber = numbers.removeValue(forKey: 6)
+print("Removed value is:", removedNumber)
+
+// contains will provide a boolean response if the key is available or not in the dictionary
+// contains require a key as parameter
+// it will return true if the provided key is present in the dictionary
+// it will return false if the provided key is not present in the dictionary
+isPresent = numbers.keys.contains(6)
+print("Is 6 present in dictionary", isPresent)
+
+// remove all elements in dictionary
+numbers.removeAll()
+print(numbers)
+
+
+// we can try later
+// shuffled()
+// randomElement()
+// firstIndex()
