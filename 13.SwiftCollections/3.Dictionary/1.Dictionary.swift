eb766e95e00e27670d9b05b214711f8980ab35f0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-01 23:02:04 +0600

    dictionary

---
+++ 13.SwiftCollections/3.Dictionary/1.Dictionary.swift
@@ -0,0 +1,17 @@
+
+
+// Dictionary is a special kind of collection
+// Which arrange data in key-value pairs manner
+// for every key, there will be some value
+// that's why this is called key-value pairs
+
+
+// Swift dictionary is an unordered collection of items.
+
+
+// this is a dictionary
+// in which 
+// key is the country name
+// value is the capital name
+var capitalCity = ["Nepal": "Kathmandu", "Italy": "Rome", "England": "London"]
+print(capitalCity)
