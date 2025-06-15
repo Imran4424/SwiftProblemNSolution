3baa5a7074807207e35db9db0e4d7f6993841aa3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-15 22:15:25 +0600

    enum

---
+++ 19.Enum/1.Enum.swift
@@ -0,0 +1,6 @@
+
+// like struct and class, we can also declare user-defined data type using enum
+// while class is reference type, struct and enum is value type
+
+
+// while struct and class provides a composite data type
