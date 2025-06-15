369544a6b794550ca7beb535c3ef01b3f9148416
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-15 22:52:30 +0600

    enum

--- 19.Enum/1.Enum.swift
+++ 19.Enum/1.Enum.swift
@@ -5,3 +5,60 @@
 
 // while struct and class provides a composite data type (collection of fundamental or user defined properties)
 // enum focuses on group of related values
+
+enum Weekday {
+        case monday
+        case tuesday
+        case wednesday
+        case thursday
+        case friday
+        case saturday
+        case sunday 
+}
+
+// another basic enum syntax is
+
+enum CompassDirection {
+        case north
+        case south
+        case east
+        case west
+}
+
+func shareYourStatus(on day: Weekday) {
+        switch day {
+        case .monday:
+                print("Go to Office")
+        case .tuesday:
+                print("Go to Office")
+        case .wednesday:
+                print("Go to Office")
+        case .thursday:
+                print("Go to Office")
+        case .friday:
+                print("Go to Office")
+        case .saturday:
+                print("It's weekend, Relax and study")
+        case .sunday:
+                print("It's weekend, Relax and study")
+        }
+}
+
+func navigate(to direction: CompassDirection) {
+        switch direction {
+        case .north:
+                print("Go to north")
+        case .south:
+                print("Go to south")
+        case .east:
+                print("Go to east")
+        case .west:
+                print("Go to west")
+        }
+}
+
+shareYourStatus(on: .monday)
+shareYourStatus(on: .sunday)
+
+navigate(to: .north)
+navigate(to: .west)
---
+++ 19.Enum/2.Constants.swift
@@ -0,0 +1,4 @@
+
+// In previous code we learned about enum basics
+
+// in this code we gonna learn about enum constants
