75a0e17ea5d1d2623046aa4445d34e4dd816e318
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-17 01:37:18 +0600

    enum

--- 19.Enum/5.AssociatedValues.swift
+++ 19.Enum/5.AssociatedValues.swift
@@ -5,3 +5,23 @@ enum SocialMedia {
         case instagram
         case linkedIn
 }
+
+func getSponsorshipEligibility(for platform: SocialMedia) {
+        switch platform {
+        case .twitter(let followers):
+                if followers > 10000 {
+                        print("Eligible for sponsored twitter")
+                } else {
+                        print("Not Eligible for sponsorship")
+                }
+                
+        case .youtube(let subscribers) where subscribers > 25000:
+                print("Eligible for sponsored youtube")
+
+        case .instagram, .youtube:
+                print("Not Eligible for sponsorship")
+
+        case .linkedIn:
+                print("Not Eligible for sponsorship")
+        }
+}
