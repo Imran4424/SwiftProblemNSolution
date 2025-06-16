fc119ed26c703231c3638885951809b638c2df3e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-17 00:01:30 +0600

    enum

--- 19.Enum/2.Constants.swift
+++ 19.Enum/2.Constants.swift
@@ -19,12 +19,19 @@ func set(itemInfoType: ItemInfoType) {
         switch itemInfoType {
         case .location:
                 print(SFSymbols.location)
+
         case .repos:
                 print(SFSymbols.repos)
+
+        case .gists:
+                print(SFSymbols.gists)
+        
         case .followers:
                 print(SFSymbols.followers)
+
         case .following:
                 print(SFSymbols.following)
         }
 }
 
+set(itemInfoType: .followers)
