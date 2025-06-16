f42ff92c74c0c296915477330668e8a8b9fce4ed
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-16 23:47:20 +0600

    enum

--- 19.Enum/2.Constants.swift
+++ 19.Enum/2.Constants.swift
@@ -2,3 +2,11 @@
 // In previous code we learned about enum basics
 
 // in this code we gonna learn about enum constants
+
+enum SFSymbols {
+        static let location = "mappin.and.ellipse"
+        static let repos = "folder"
+        static let gists = "text.alignleft"
+        static let followers = "heart"
+        static let following = "person.2"
+}
