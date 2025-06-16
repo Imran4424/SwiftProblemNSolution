2f10fd4f6e042066ef1b6272cad5e670c7e2b67b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-16 23:51:52 +0600

    enum

--- 19.Enum/2.Constants.swift
+++ 19.Enum/2.Constants.swift
@@ -3,6 +3,10 @@
 
 // in this code we gonna learn about enum constants
 
+enum ItemInfoType {
+        case location, repos, gists, followers, following
+}
+
 enum SFSymbols {
         static let location = "mappin.and.ellipse"
         static let repos = "folder"
@@ -10,3 +14,17 @@ enum SFSymbols {
         static let followers = "heart"
         static let following = "person.2"
 }
+
+func set(itemInfoType: ItemInfoType) {
+        switch itemInfoType {
+        case .location:
+                print(SFSymbols.location)
+        case .repos:
+                print(SFSymbols.repos)
+        case .followers:
+                print(SFSymbols.followers)
+        case .following:
+                print(SFSymbols.following)
+        }
+}
+
