ccf1ccbea26af88a982482b8906a8a441690f58c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-26 23:00:56 +0600

    access control

--- 22.AccessControl/CodeSpace/1.Module.swift
+++ 22.AccessControl/CodeSpace/1.Module.swift
@@ -1,1 +1,6 @@
 
+// Swift’s access control model is based on the concept of modules, source files, and packages.
+
+
+// A module is a single unit of code distribution — a framework or application that’s built and shipped 
+// as a single unit and that can be imported by another module with Swift’s import keyword.
