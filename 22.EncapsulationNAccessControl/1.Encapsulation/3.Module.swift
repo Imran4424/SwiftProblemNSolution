c00a7f8b7600f7f4babf532d0ec0b827dc204a93
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-27 00:38:32 +0600

    access control

--- 22.EncapsulationNAccessControl/1.Encapsulation/3.Module.swift
+++ 22.EncapsulationNAccessControl/1.Encapsulation/3.Module.swift
@@ -2,5 +2,7 @@
 // Swift’s access control model is based on the concept of modules, source files, and packages.
 
 
-// A module is a single unit of code distribution — a framework or application that’s built and shipped 
-// as a single unit and that can be imported by another module with Swift’s import keyword.
+// A module is a single unit of code distribution — 
+// 
+// a framework or application that’s built and shipped as a single unit and that can be imported by 
+// another module with Swift’s import keyword.
