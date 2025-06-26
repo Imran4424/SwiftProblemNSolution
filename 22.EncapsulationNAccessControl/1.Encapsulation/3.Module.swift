e85993830a5c29e9688ad850fe1fa9237b07530e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-27 00:43:08 +0600

    access control

--- 22.EncapsulationNAccessControl/1.Encapsulation/3.Module.swift
+++ 22.EncapsulationNAccessControl/1.Encapsulation/3.Module.swift
@@ -6,3 +6,5 @@
 // 
 // a framework or application that’s built and shipped as a single unit and that can be imported by 
 // another module with Swift’s import keyword.
+
+// Each build target (such as an app bundle or framework) in Xcode is treated as a separate module in Swift.
