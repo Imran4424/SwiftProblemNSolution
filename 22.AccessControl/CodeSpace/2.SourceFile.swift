537f700f3e2f83b9a28ddfac067e27d607095ddd
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-26 23:35:23 +0600

    access control

---
+++ 22.AccessControl/CodeSpace/2.SourceFile.swift
@@ -0,0 +1,62 @@
+
+// Swift’s access control model is based on the concept of modules, source files, and packages.
+
+// A source file is a single Swift source code file within a module (in effect, a single file within an app or framework). 
+// Although it’s common to define individual types in separate source files, 
+// 
+// a single source file can contain definitions for multiple types, functions, and so on.
+
+struct Point {
+        var x = 0.0, y = 0.0
+}
+
+struct Size {
+        var width = 0.0, height = 0.0
+}
+
+struct Rect {
+        var origin = Point()
+        var size = Size()
+
+        var center: Point {
+                // getter - returns the computed value
+                get {
+                        let centerX = origin.x + (size.width / 2)
+                        let centerY = origin.y + (size.height / 2)
+                        return Point(x: centerX, y: centerY)
+                }
+
+                // when we are setting a new center 
+                // that means the origin and size will be different too
+                // in this case we kep size unchanged
+                // since center is changed we need to calculate the origin in this case
+                // setter - changes the value
+                set(newCenter) {
+                        origin.x = newCenter.x - (size.width / 2)
+                        origin.y = newCenter.y - (size.height / 2)
+                }
+        }
+}
+
+var square = Rect(origin: Point(x: 0.0, y: 0.0),
+                  size: Size(width: 10.0, height: 10.0))
+
+// Getting stored property
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+
+// Getting computed property
+// this will call get block of computed property
+print("square.center is now at  (\(square.center.x), \(square.center.y))")
+let initialSquareCenter = square.center
+
+// Setting computed property
+// this will call the set block of computed property
+square.center = Point(x: 15.0, y: 15.0)
+
+// Prints "square.origin is now at (10.0, 10.0)"
+// Getting stored property
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+
+// Getting computed property
+// this will call get block of computed property
+print("square.center is now at  (\(square.center.x), \(square.center.y))")
