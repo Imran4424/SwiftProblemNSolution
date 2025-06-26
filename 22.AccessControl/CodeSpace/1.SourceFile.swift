ed891c3805080d15ef554d4f56e2c921f115f4eb
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-26 23:33:00 +0600

    access control

--- 22.AccessControl/CodeSpace/1.SourceFile.swift
+++ 22.AccessControl/CodeSpace/1.SourceFile.swift
@@ -2,5 +2,7 @@
 // Swift’s access control model is based on the concept of modules, source files, and packages.
 
 // A source file is a single Swift source code file within a module (in effect, a single file within an app or framework). 
-// Although it’s common to define individual types in separate source files, a single source file can contain 
-// definitions for multiple types, functions, and so on.
+// Although it’s common to define individual types in separate source files, 
+// 
+// a single source file can contain definitions for multiple types, functions, and so on.
+
