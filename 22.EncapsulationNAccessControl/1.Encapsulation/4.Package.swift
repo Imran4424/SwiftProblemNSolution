ddd71da20d05ed1c52dc3f8bc2d1d35c928c5031
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-27 22:54:07 +0600

    access control

--- 22.EncapsulationNAccessControl/1.Encapsulation/4.Package.swift
+++ 22.EncapsulationNAccessControl/1.Encapsulation/4.Package.swift
@@ -2,3 +2,5 @@
 // Swift’s access control model is based on the concept of modules, source files, and packages.
 
 // A package is a group of modules that you develop as a unit.
+
+// in a package module count can vary from one to more
