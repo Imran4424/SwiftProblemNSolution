315ea3f87d95092643fbd0b9408d81d26d339d2d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-30 00:08:13 +0600

    access control

--- 22.EncapsulationNAccessControl/9.Open/BoxExtension.swift
+++ 22.EncapsulationNAccessControl/9.Open/BoxExtension.swift
@@ -2,18 +2,16 @@
 // extension can not be open
 // specify the extension access control as public
 // then make it's properties and methods open if you want to make them open
-open extension Box {
-        // these are implicitly open
-        // because the outer code block is open
-        func modifyProperties(length: Int, width: Int, height: Int) {
+public extension Box {
+        // explicitly open
+        open func modifyProperties(length: Int, width: Int, height: Int) {
                 self.length = length
                 self.width = width
                 self.height = height
         }
 
-        // these are implicitly open
-        // because the outer code block is open
-        func displayInfo() {
+        // explicitly open
+        open func displayInfo() {
                 print("Box length:", length)
                 print("Box width:", width)
                 print("Box height:", height)
