be56066b6678c8c5e804c7659ce89f8013765111
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-06-30 00:07:36 +0600

    access control

--- 22.EncapsulationNAccessControl/9.Open/BoxExtension.swift
+++ 22.EncapsulationNAccessControl/9.Open/BoxExtension.swift
@@ -1,5 +1,7 @@
 
-// explicitly open
+// extension can not be open
+// specify the extension access control as public
+// then make it's properties and methods open if you want to make them open
 open extension Box {
         // these are implicitly open
         // because the outer code block is open
