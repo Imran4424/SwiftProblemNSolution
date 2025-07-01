458e752401f09dc0f623202882be5b44414d70d7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-01 15:27:26 +0600

    access control

---
+++ 22.EncapsulationNAccessControl/10.MultiLevel/4.PublicPrivateInternal/BoxExtension.swift
@@ -0,0 +1,45 @@
+
+// extension not in same source file can not access private elements
+// this extension is not in the same source file as defined type
+
+// explicitly public
+public extension Box {
+
+        // since the class in public
+        // the following code is also public implicity
+        func modifyProperties(length: Int, width: Int, height: Int) {
+                // the following codes will give compilation error 
+                // since all the elements accessed here is private
+                // although this is the class extension and we should able to access the private values
+                // but this extension is not in the same file as the defined type
+                // we can not access them
+                // self.length = length
+                // self.width = width
+                // self.height = height
+
+                setLength(length: length)
+                setWidth(width: width)
+                setHeight(height: height)
+        }
+
+        // since the class in public
+        // the following code is also public implicity
+        func displayInfo() {
+                // the following codes will give compilation error 
+                // since all the elements accessed here is private
+                // although this is the class extension and we should able to access the private values
+                // but this extension is not in the same file as the defined type
+                // we can not access them
+                // print("Box length:", length)
+                // print("Box width:", width)
+                // print("Box height:", height)
+                // print("Box Area:", length * width)
+                // print("Box Volume", length * width * height)
+
+                print("Box length:", getLength())
+                print("Box width:", getWidth())
+                print("Box height:", getHeight())
+                print("Box Area:", area())
+                print("Box Volume", volume())
+        }
+}
