e6493cb1fca91b5d3362855fb230c63f0467ce62
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-01 15:40:00 +0600

    access control

--- 22.EncapsulationNAccessControl/10.MultiLevel/5.PublicInternalSet/Box.swift
+++ 22.EncapsulationNAccessControl/10.MultiLevel/5.PublicInternalSet/Box.swift
@@ -57,14 +57,7 @@ public class Box
                 self.width = width
         }
 
-        // getters for width
-        // since the class in public
-        // the following code is also public implicity
-        // but by using internal explicitly we restrict the access level from public to internal
-        // we can do this type of modification from least restrictive to more restrictive access control
-        internal func getWidth() -> Int {
-                return width
-        }
+        
 
         // setters for height
         // since the class in public
