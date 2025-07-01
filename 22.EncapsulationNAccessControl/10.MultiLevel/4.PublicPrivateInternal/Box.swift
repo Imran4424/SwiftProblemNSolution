8c477dfd48d8daaae19a9debbb85e7bf9a6d41c5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-01 15:29:35 +0600

    access control

--- 22.EncapsulationNAccessControl/10.MultiLevel/4.PublicPrivateInternal/Box.swift
+++ 22.EncapsulationNAccessControl/10.MultiLevel/4.PublicPrivateInternal/Box.swift
@@ -33,7 +33,7 @@ public class Box
         // the following code is also public implicity
         // but by using internal explicitly we restrict the access level from public to internal
         // we can do this type of modification from least restrictive to more restrictive access control
-        init(length: Int, width: Int, height: Int) {
+        internal init(length: Int, width: Int, height: Int) {
                 self.length = length
                 self.width = width
                 self.height = height
