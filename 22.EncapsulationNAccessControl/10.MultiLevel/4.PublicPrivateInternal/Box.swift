175ef7edd17ba621cb137812d6e07d370fdc3904
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-01 15:32:24 +0600

    access control

--- 22.EncapsulationNAccessControl/10.MultiLevel/4.PublicPrivateInternal/Box.swift
+++ 22.EncapsulationNAccessControl/10.MultiLevel/4.PublicPrivateInternal/Box.swift
@@ -87,6 +87,8 @@ public class Box
         // getters for height
         // since the class in public
         // the following code is also public implicity
+        // but by using internal explicitly we restrict the access level from public to internal
+        // we can do this type of modification from least restrictive to more restrictive access control
         func getHeight() -> Int {
                 return height
         }
