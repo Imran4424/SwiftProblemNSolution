590057da220549e26e53c6dfe64356c59f8fb7e5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-01 15:31:51 +0600

    access control

--- 22.EncapsulationNAccessControl/10.MultiLevel/4.PublicPrivateInternal/Box.swift
+++ 22.EncapsulationNAccessControl/10.MultiLevel/4.PublicPrivateInternal/Box.swift
@@ -78,6 +78,8 @@ public class Box
         // setters for height
         // since the class in public
         // the following code is also public implicity
+        // but by using internal explicitly we restrict the access level from public to internal
+        // we can do this type of modification from least restrictive to more restrictive access control
         func setHeight(height: Int) {
                 self.height = height
         }
