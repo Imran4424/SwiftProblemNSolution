8def2b41df92e0df8aed2d867f050a54649bee27
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-12 20:50:53 +0600

    inheritance

---
+++ 23.Inheritance/1.ClassInheritance/2.SingleInheritance.swift
@@ -0,0 +1,51 @@
+
+
+// In case of intheritance
+// If you want the properties or methods to be inherited
+// don't make them private
+// internal is the best access modifier for the inter module inheritance
+
+// Super class
+class Person {
+        var name: String
+        var age: Int
+
+        init(name: String, age: Int) {
+                // self keyword is for indicating object address
+                // self.name indicates the object property
+                // name indicates the passed parameter of init
+                // same goes for age
+                self.name = name
+                self.age = age
+        }
+}
+
+class Student: Person {
+        var studentID: Int
+        var institution: String
+
+        init(name: String, age: Int, studentID: Int, institution: String) {
+                self.studentID = studentID
+                self.institution = institution
+                super.init(name: name, age: age)
+        }
+
+        func displayInfo() {
+                print("------Student------")
+                print("Name:", name)
+                print("Age:", age)
+                print("Student ID:", studentID)
+                print("Institution:", institution)
+        }
+}
+
+var pritha = Student(name: "Pritha Saha", age: 20, studentID: 1917, institution: "Mac Masters")
+pritha.displayInfo()
+
+
+/*
+	single inheritance is really simple 
+
+
+	in single inheritance pattern there will be one Super class and One Sub Class class
+*/
