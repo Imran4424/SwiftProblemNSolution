585402343036c3b53045bf0450169050b546a7dd
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-12 23:16:29 +0600

    inheritance

--- 23.Inheritance/1.ClassInheritance/5.MultiLevelInheritance.swift
+++ 23.Inheritance/1.ClassInheritance/5.MultiLevelInheritance.swift
@@ -83,6 +83,6 @@ sazim.displayInfo()
 
         Person is the super class
 
-        Student is the sub class of Person, but super 
+        Student is the sub class of Person, but super class 
 
 */
