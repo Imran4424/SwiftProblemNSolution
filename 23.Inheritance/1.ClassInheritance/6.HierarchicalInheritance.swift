24f09b30d190c978e5b129ba9ebaaef16ebe9652
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-13 10:05:31 +0600

    inheritance

--- 23.Inheritance/1.ClassInheritance/6.HierarchicalInheritance.swift
+++ 23.Inheritance/1.ClassInheritance/6.HierarchicalInheritance.swift
@@ -4,3 +4,37 @@
 // don't make them private
 // internal is the best access modifier for the inter module inheritance
 
+// Super class
+class Person {
+        var name: String
+        var age: Int
+
+        init(name: String, age: Int) {
+                // self keyword is for indicating object address
+                // self.name indicates the object property
+                // name indicates the passed parameter of init
+                // same goes for age
+                self.name = name
+                self.age = age
+        }
+}
+
+// sub class 1
+class Student: Person {
+        var studentID: Int
+        var institution: String
+
+        init(name: String, age: Int, studentID: Int, institution: String) {
+                self.studentID = studentID
+                self.institution = institution
+                super.init(name: name, age: age)
+        }
+
+        func displayInfo() {
+                print("------Student------")
+                print("Name:", name)
+                print("Age:", age)
+                print("Student ID:", studentID)
+                print("Institution:", institution)
+        }
+}
