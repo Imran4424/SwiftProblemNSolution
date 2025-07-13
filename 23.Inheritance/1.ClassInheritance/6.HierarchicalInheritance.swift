9a47afe210f2643790efb447f67ca2fd73952841
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-13 09:59:06 +0600

    inheritance

--- 23.Inheritance/1.ClassInheritance/6.HierarchicalInheritance.swift
+++ 23.Inheritance/1.ClassInheritance/6.HierarchicalInheritance.swift
@@ -1,1 +1,6 @@
 
+// In case of intheritance
+// If you want the properties or methods to be inherited
+// don't make them private
+// internal is the best access modifier for the inter module inheritance
+
