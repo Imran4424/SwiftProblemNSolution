4e76de19700a1853ad166557d932c189cfa575c9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-16 21:27:16 +0600

    inheritance

---
+++ 23.Inheritance/1.ClassInheritance/8.MultipleInheritance.swift
@@ -0,0 +1,41 @@
+
+// In case of intheritance
+// If you want the properties or methods to be inherited
+// don't make them private
+// internal is the best access modifier for the inter module inheritance
+
+// person protocol
+protocol Person {
+        var name: String { get set }
+        var age: Int { get set }
+}
+
+protocol Employee {
+        var wage: Int { get set }
+        func displayInfo()
+}
+
+class Teacher: Person, Employee {
+        var name: String
+        var age: Int
+        var wage: Int
+        var institution: String
+
+        init(name: String, age: Int, wage: Int, institution: String) {
+                self.name = name
+                self.age = age
+                self.wage = wage
+                self.institution = institution
+        }
+
+        func displayInfo() {
+                print("------Teacher------")
+                print("Name:", name)
+                print("Age:", age)
+                print("Wage:", wage)
+                print("Institution:", institution)
+        }
+}
+
+var imran = Teacher(name: "SHAH MD. IMRAN HOSSAIN", age: 27, wage: 20000, institution: "Kennesaw State University")
+imran.displayInfo()
