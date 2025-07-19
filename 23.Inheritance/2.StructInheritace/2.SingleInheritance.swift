798aa6b68951f40813ee7b2a5f5e638521e496da
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-19 16:48:58 +0600

    inheritance

---
+++ 23.Inheritance/2.StructInheritace/2.SingleInheritance.swift
@@ -0,0 +1,58 @@
+
+// in this series we are gonna learn about struct inheritance
+// the persons who have OOP knowledge from other languages like C++, Java
+// they can be surprised about hearing the term because other language don't support struct inheritance
+
+// Swift is the first language to bring Protocol Oriented Paradigm
+// and
+// one of the best features of Protocol Oriented Programming(POP) is
+// it enables the inheritance functionality of Any types (not just for classes)
+// Class, Struct (structure), Enum (enumeration ) all can use protocol based inheritance
+
+
+// Super struct
+protocol Person {
+        var name: String
+        var age: Int
+}
+
+// sub struct
+// but the following code will give compilation error
+// because struct only supports protocol based inheritance
+// so, inheritance from other than protocol (such as struct or class(which is super unlikely since they are not same type)) 
+// will result in compilation error
+//
+// error message: inheritance from non-protocol type 'Person'
+struct Student: Person {
+        var studentID: Int
+        var institution: String
+
+        init(name: String, age: Int, studentID: Int, institution: String) {
+                self.studentID = studentID
+                self.institution = institution
+                // super is related to class inheritance
+                // so it will also give compilation error
+                super.init(name: name, age: age)
+        }
+
+        func displayInfo() {
+                print("------Student------")
+                print("Name:", name)
+                print("Age:", age)
+                print("Student ID:", studentID)
+                print("Institution:", institution)
+        }
+}
+
+var pritha = Student(name: "Pritha Saha", age: 20, studentID: 1917, institution: "Mac Masters")
+pritha.displayInfo()
+
+
+/* 
+        In case of struct inheritance
+        struct does not support other type of inheritance other than protocol based inheritance
+
+        In struct single inheritance
+
+        There will be one base protocol and one struct which confrom the protcol
+*/
