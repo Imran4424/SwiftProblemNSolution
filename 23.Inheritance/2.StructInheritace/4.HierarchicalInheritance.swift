59e4b9493bfd6f9b82540dc6b3340591039497c7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-20 00:20:22 +0600

    inheritance

--- 23.Inheritance/2.StructInheritace/4.HierarchicalInheritance.swift
+++ 23.Inheritance/2.StructInheritace/4.HierarchicalInheritance.swift
@@ -1,4 +1,14 @@
 
+// in this series we are gonna learn about struct inheritance
+// the persons who have OOP knowledge from other languages like C++, Java
+// they can be surprised about hearing the term because other language don't support struct inheritance
+
+// Swift is the first language to bring Protocol Oriented Paradigm
+// and
+// one of the best features of Protocol Oriented Programming(POP) is
+// it enables the inheritance functionality of Any types (not just for classes)
+// Class, Struct (structure), Enum (enumeration ) all can use protocol based inheritance
+
 // person protocol
 protocol Person {
         var name: String { get set }
