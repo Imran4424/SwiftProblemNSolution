fa474ddee90d27e25893b6201efdad8cfcc91dec
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-20 00:10:51 +0600

    inheritance

--- 23.Inheritance/2.StructInheritace/4.HierarchicalInheritance.swift
+++ 23.Inheritance/2.StructInheritace/4.HierarchicalInheritance.swift
@@ -4,3 +4,88 @@ protocol Person {
         var name: String { get set }
         var age: Int { get set }
 }
+
+// sub class 1
+class Student: Person {
+        var studentID: Int
+        var institution: String
+
+        init(name: String, age: Int, studentID: Int, institution: String) {
+                self.name = name
+                self.age = age
+                self.studentID = studentID
+                self.institution = institution
+        }
+
+        func displayInfo() {
+                print("------Student------")
+                print("Name:", name)
+                print("Age:", age)
+                print("Student ID:", studentID)
+                print("Institution:", institution)
+                print()
+        }
+}
+
+// sub class 2
+class Engineer: Person {
+        var department: String
+
+        init(name: String, age: Int, department: String) {
+                self.department = department
+                super.init(name: name, age: age)
+        }
+
+        func displayInfo() {
+                print("------Engineer------")
+                print("Name:", name)
+                print("Age:", age)
+                print("Department:", department)
+                print()
+        }
+}
+
+// sub class 3
+class Doctor: Person {
+        var speciality: String
+
+        init(name: String, age: Int, speciality: String) {
+                self.speciality = speciality
+                super.init(name: name, age: age)
+        }
+
+        func displayInfo() {
+                print("------Doctor------")
+                print("Name:", name)
+                print("Age:", age)
+                print("Speciality:", speciality)
+                print()
+        }
+}
+
+var pritha = Student(name: "Pritha Saha", age: 20, studentID: 1917, institution: "Mac Masters")
+pritha.displayInfo()
+
+var imran = Engineer(name: "Shah Md. Imran Hossain", age: 27, department: "Computer Science")
+imran.displayInfo()
+
+var soudha = Doctor(name: "Shira Jahan Soudha", age: 27, speciality: "Endocrinology & Metabolism")
+soudha.displayInfo()
+
+/*
+	This is Hierachical Inheritance
+
+        In Hierachical Inheritance
+        There will be one super class and multiple sub classes
+*/
+
+/*  
+        In this Hierachical Inheritance
+        There is one super class and three sub classes
+
+        Person - super class
+
+        Student - sub class one
+        Engineer - sub class two
+        Doctor - sub class three
+*/
