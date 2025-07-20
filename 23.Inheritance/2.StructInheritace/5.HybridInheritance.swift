81a6b4bf34f3e31667d292b340b517be64b26a15
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-20 18:14:07 +0600

    inheritance

--- 23.Inheritance/2.StructInheritace/5.HybridInheritance.swift
+++ 23.Inheritance/2.StructInheritace/5.HybridInheritance.swift
@@ -8,3 +8,118 @@
 // one of the best features of Protocol Oriented Programming(POP) is
 // it enables the inheritance functionality of Any types (not just for classes)
 // Class, Struct (structure), Enum (enumeration ) all can use protocol based inheritance
+
+// person protocol
+protocol Person {
+        var name: String { get set }
+        var age: Int { get set }
+}
+
+// Employee protocol
+protocol Employee {
+        var wage: Int { get set }
+        func displayInfo()
+}
+
+// Multiple Inheritance through protcols
+// Conforming multiple protocols
+struct Teacher: Person, Employee {
+        var name: String
+        var age: Int
+        var wage: Int
+        var institution: String
+
+        init(name: String, age: Int, wage: Int, institution: String) {
+                self.name = name
+                self.age = age
+                self.wage = wage
+                self.institution = institution
+        }
+
+        func displayInfo() {
+                print("------Teacher------")
+                print("Name:", name)
+                print("Age:", age)
+                print("Wage:", wage)
+                print("Institution:", institution)
+        }
+}
+
+// struct one
+struct Student: Person {
+        var name: String
+        var age: Int
+        var studentID: Int
+        var institution: String
+
+        init(name: String, age: Int, studentID: Int, institution: String) {
+                self.name = name
+                self.age = age
+                self.studentID = studentID
+                self.institution = institution
+        }
+
+        func displayInfo() {
+                print("------Student------")
+                print("Name:", name)
+                print("Age:", age)
+                print("Student ID:", studentID)
+                print("Institution:", institution)
+                print()
+        }
+}
+
+// struct two
+struct Engineer: Person {
+        var name: String
+        var age: Int
+        var department: String
+
+        init(name: String, age: Int, department: String) {
+                self.name = name
+                self.age = age
+                self.department = department
+        }
+
+        func displayInfo() {
+                print("------Engineer------")
+                print("Name:", name)
+                print("Age:", age)
+                print("Department:", department)
+                print()
+        }
+}
+
+// struct three
+struct Doctor: Person {
+        var name: String
+        var age: Int
+        var speciality: String
+
+        init(name: String, age: Int, speciality: String) {
+                self.name = name
+                self.age = age
+                self.speciality = speciality
+        }
+
+        func displayInfo() {
+                print("------Doctor------")
+                print("Name:", name)
+                print("Age:", age)
+                print("Speciality:", speciality)
+                print()
+        }
+}
+
+var pritha = Student(name: "Pritha Saha", age: 20, studentID: 1917, institution: "Mac Masters")
+pritha.displayInfo()
+
+var imranCoder = Engineer(name: "Shah Md. Imran Hossain", age: 27, department: "Computer Science")
+imranCoder.displayInfo()
+
+var soudha = Doctor(name: "Shira Jahan Soudha", age: 27, speciality: "Endocrinology & Metabolism")
+soudha.displayInfo()
+
+
+var imran = Teacher(name: "SHAH MD. IMRAN HOSSAIN", age: 27, wage: 20000, institution: "Kennesaw State University")
+imran.displayInfo()
