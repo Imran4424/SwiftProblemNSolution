9c5f0806fe3fb7684af937bdacfd3491ab6199d8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-21 00:24:03 +0600

    polymorphism

--- 24.Polymorphism/1.Overloading/1.FunctionOverloading/4.FunctionOverloadingWithDefaultValues.swift
+++ 24.Polymorphism/1.Overloading/1.FunctionOverloading/4.FunctionOverloadingWithDefaultValues.swift
@@ -5,11 +5,11 @@
 // and based on default values
 
 
-func add(x: Int, y: Int, z: Int) -> Int {
+func add(x: Int = 0, y: Int = 0, z: Int = 0) -> Int {
         return x + y + z
 }
 
-func add(x: Double, y: Double, z: Double) -> Double {
+func add(x: Double = 0.0, y: Double = 0.0, z: Double = 0.0) -> Double {
         return x + y + z
 }
 
