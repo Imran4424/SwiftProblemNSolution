29c8275323bcb0fde8e1de97df462a74258fcbf7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-20 23:51:21 +0600

    polymorphism

---
+++ 24.Polymorphism/1.Overloading/1.FunctionOverloading/2.FunctionOverloadingCompilation.swift
@@ -0,0 +1,28 @@
+
+
+// polymorphism
+// based on parameter type
+
+func add(x: Int, y: Int) -> Int {
+        return x + y
+}
+
+// func add(x: Double, y: Double) -> Double {
+//         return x + y
+// }
+
+func add(x: Int, y: Int, z: Int) -> Int {
+        return x + y + z
+}
+
+func add(x: Double, y: Double, z: Double) -> Double {
+        return x + y + z
+}
+
+print("Sum of two values:", add(x: 5, y: 7))
+
+print("Sum of two values:", add(x: 5.7, y: 7.5))
+
+print("Sum of three values:", add(x: 12, y: 13, z: 15))
+
+print("Sum of three values:", add(x: 12.13, y: 13.15, z: 15.12))
