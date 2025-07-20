497d1d5b3c681a74b273c02191fbebb0056bbfd6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-20 23:31:02 +0600

    polymorphism

--- 24.Polymorphism/1.Overloading/1.FunctionOverloading/1.FunctionOverloading.swift
+++ 24.Polymorphism/1.Overloading/1.FunctionOverloading/1.FunctionOverloading.swift
@@ -1,1 +1,11 @@
 
+// polymorphism
+// based on number parameter count
+
+func add(x: Int, y: Int) -> Int {
+        return x + y
+}
+
+func add(x: Int, y: Int, z: Int) -> Int {
+        return x + y + z
+}
