8aa2bdab0718744e4e43a543f2e8703c780c1e92
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-21 20:40:59 +0600

    polymorphism

---
+++ 24.Polymorphism/1.Overloading/1.FunctionOverloading/6.FunctionOverloadingWithGenerics.swift
@@ -0,0 +1,22 @@
+
+
+// polymorphism
+// based on parameter count
+// based on generics
+// and based on default values
+
+func add<T: AdditiveArithmetic>(x: T, y: T) -> T {
+        return x + y
+}
+
+func add<T: AdditiveArithmetic>(x: T, y: T, z: T) -> T {
+        return x + y + z
+}
+
+print("Sum of two values:", add(x: 5, y: 7))
+
+print("Sum of two values:", add(x: 5.7, y: 7.5))
+
+print("Sum of three values:", add(x: 12, y: 13, z: 15))
+
+print("Sum of three values:", add(x: 12.13, y: 13.15, z: 15.12))
