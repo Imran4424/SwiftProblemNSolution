97e414c3360c803ac4b47111ec8e3d84646f2b5f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-21 20:46:43 +0600

    polymorphism

--- 24.Polymorphism/1.Overloading/1.FunctionOverloading/7.FunctionOverloadingWithGenericsCompilation.swift
+++ 24.Polymorphism/1.Overloading/1.FunctionOverloading/7.FunctionOverloadingWithGenericsCompilation.swift
@@ -8,6 +8,8 @@
 // .zero and 0 is not same
 // 0 - is Int value
 // .zero is a property which provides generic 0 value for arithmatic in this case
+//
+// error message: default argument value of type 'Int' cannot be converted to type 'T'
 func add<T: AdditiveArithmetic>(x: T = 0, y: T = 0, z: T = 0) -> T {
         return x + y + z
 }
