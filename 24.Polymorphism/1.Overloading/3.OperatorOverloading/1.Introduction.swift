0b2e60c905df009edb94fb96b68d4ad6e214d000
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-22 23:08:10 +0600

    polymorphism

--- 24.Polymorphism/1.Overloading/3.OperatorOverloading/1.Introduction.swift
+++ 24.Polymorphism/1.Overloading/3.OperatorOverloading/1.Introduction.swift
@@ -1,3 +1,15 @@
 
 // polymorphism
 // operator overloading
+
+/* 
+        operator overloading is a technique to introduce new custom operators
+
+        or
+
+        add new implementation to existing operators
+
+
+        In this series,
+        First, we gonna learn how to add new 
+*/
