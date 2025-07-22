e8b86c34b35acea8f7045910a9162fe1ebb479d8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-22 22:52:50 +0600

    polymorphism

---
+++ 24.Polymorphism/1.Overloading/2.MethodOverloading/5.MethodOverloading.swift
@@ -0,0 +1,10 @@
+
+class Calculator {
+        func add(x: Int = 0, y: Int = 0, z: Int = 0) -> Int {
+                return x + y + z
+        }
+
+        func add(x: Double = 0.0, y: Double = 0.0, z: Double = 0.0) -> Double {
+                return x + y + z
+        }
+}
