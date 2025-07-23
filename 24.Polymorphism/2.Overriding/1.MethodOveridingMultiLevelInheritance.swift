c0171bf8693b5706b6c0f54daa74207782726707
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-23 22:34:45 +0600

    polymorphism

--- 24.Polymorphism/2.Overriding/1.MethodOveridingMultiLevelInheritance.swift
+++ 24.Polymorphism/2.Overriding/1.MethodOveridingMultiLevelInheritance.swift
@@ -1,9 +1,5 @@
 
 
-// In case of intheritance
-// If you want the properties or methods to be inherited
-// don't make them private
-// internal is the best access modifier for the inter module inheritance
 
 class Person {
         var name: String
@@ -78,52 +74,3 @@ var sazim = SchoolStudent(
 
 sazim.displayInfo()
 
-/* 
-        This is multi-level inheritance
-
-        Here,
-        Person is the super class for Student
-        Student is the sub class of the Person, but super class for SchoolStudent
-        SchoolStudent is the sub class of the Student
-
-        There is an another common term of relationship for multi-level inheritance, Grandparent, parent, child
-
-        Here, in this case
-        Person is Parent of Student, Grandparent of SchoolStudent
-        Student is Child of Person, Parent of SchoolStudent
-        SchoolStudent is child of Student, Grandchild of Person
-
-        Funny right
-        Since, multi-level inheritance can go further, this relation term can also further.
-        for example: great great grandparent to great great grandchild 
-*/
-
-/* 
-        As you can see,
-
-        in class level inheritance, even the parent class can have independent object
-
-        Here, 
-        pritha is the object of Student class which act as parent of ScholStudent
-        sazim is the object of SchoolStudent, which had no child class of Multi-level inheritance
-
-        Even, Person class which had no parent can create objects too
-*/
-
-/* 
-        init execution order in multi-level inheritance
-
-        to understand this better, let's think about School Student Object
-
-        For creating SchoolStudent object
-
-        1. First call SchoolStudent init() ----->
-        2. Initialize SchoolStudent properties
-        3. Then call super.init() which is Student init ----->
-        4. Initialize Student properties
-        5. Then call super.init() which is person init ----->
-        6. Initialize Person properties
-        7. Return the call to caller which is Student.init() <-----
-        8. Return the call to caller which is SchoolStudent.init() <-----
-        9. Return the created object to the caller which is SchoolStudent object <-----
-*/
