a2024b4e268e068ca134aa7a144dcf948ddcd747
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-24 00:00:40 +0600

    polymorphism

--- 24.Polymorphism/2.Overriding/1.MethodOveridingMultiLevelInheritance.swift
+++ 24.Polymorphism/2.Overriding/1.MethodOveridingMultiLevelInheritance.swift
@@ -78,5 +78,5 @@ sazim.displayInfo()
 
         When you inherit super class method but super class method implementation don't fulfill your requirement
 
-        You can always provide your wo
+        You can always provide your own
 */
