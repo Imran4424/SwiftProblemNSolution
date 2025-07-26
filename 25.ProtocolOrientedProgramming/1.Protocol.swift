4cc4d850311fe14059cb367322282fc128091406
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-26 18:17:39 +0600

    protocol oriented programming

---
+++ 25.ProtocolOrientedProgramming/1.Protocol.swift
@@ -0,0 +1,11 @@
+
+// person protocol
+protocol Person {
+        var name: String { get set }
+        var age: Int { get set }
+}
+
+/* 
+        A protocol is a special type which defines the blueprint of properties, methods and other requirements 
+        that suit a particular task or piece of functionality.
+*/
