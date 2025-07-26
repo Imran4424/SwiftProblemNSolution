52805a93190eb62ed74a8097ffbb996165afc0b4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-26 18:26:12 +0600

    protocol oriented programming

--- 25.ProtocolOrientedProgramming/1.Protocol.swift
+++ 25.ProtocolOrientedProgramming/1.Protocol.swift
@@ -8,4 +8,7 @@ protocol Person {
 /* 
         A protocol is a special type which defines the blueprint of properties, methods and other requirements 
         that suit a particular task or piece of functionality.
+
+        The protocol can then be adopted by a class, structure, or enumeration to provide an actual implementation
+        of those requirements. Any type that satisfies the requirements of a protocol is said to conform to that protocol.
 */
