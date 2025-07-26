333f9e133f1fc38b2a239440fbcbc6fa6959cd99
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-26 18:28:17 +0600

    protocol oriented programming

--- 25.ProtocolOrientedProgramming/1.Protocol.swift
+++ 25.ProtocolOrientedProgramming/1.Protocol.swift
@@ -12,5 +12,6 @@ protocol Person {
         The protocol can then be adopted by a class, structure, or enumeration to provide an actual implementation
         of those requirements. Any type that satisfies the requirements of a protocol is said to conform to that protocol.
 
-        
+        In a protocol, mostly we gonna see the defination of properties and methods. Properties will only have the 
+        type definition not any particular values and Methods will only have the deifinition (blueprint) not the body.
 */
