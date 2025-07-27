703e318e2ff60f91a8af8d3b9b9e492dd90b16b7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-27 19:31:28 +0600

    protocol oriented programming

--- 25.ProtocolOrientedProgramming/4.ProtocolExtension.swift
+++ 25.ProtocolOrientedProgramming/4.ProtocolExtension.swift
@@ -1,1 +1,20 @@
 
+/// A protocol that requires a textual description.
+protocol Describable {
+    /// A textual description of the instance.
+    var description: String { get }
+}
+
+/// An extension to the Describable protocol that provides
+/// a default implementation of a helper method.
+extension Describable {
+    
+    /// Prints the description of the object with a header.
+    ///
+    /// This default implementation can be used by any type that
+    /// conforms to `Describable` without overriding it.
+    func printDescription() {
+        print("Description: \(description)")
+    }
+}
+
