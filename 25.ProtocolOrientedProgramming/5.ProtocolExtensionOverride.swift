abe7a4db0efe4a985f167ae41467c1d335e05f60
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-07-27 19:42:39 +0600

    protocol oriented programming

---
+++ 25.ProtocolOrientedProgramming/5.ProtocolExtensionOverride.swift
@@ -0,0 +1,24 @@
+
+
+/// A protocol that requires a textual id.
+/// And implementation of identify method
+protocol Identifiable {
+    var id: String { get set }
+    func identify()
+}
+
+/// providing default implementation of identify method
+extension Identifiable {
+    func identify() {
+        print("My ID is \(id).")
+    }
+}
+
+// not giving error while struct not providing implementation
+// because protocol extension provides default implementation of identify() method
+struct User: Identifiable {
+    var id: String
+}
+
+let pritha = User(id: "prithaSaha")
+pritha.identify() // Uses the default implementation from the protocol extension
